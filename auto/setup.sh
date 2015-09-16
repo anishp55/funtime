35c0117f55e4099c296633c8e52334c853ba121d
anish k patel <akp@lp.build>
2015-09-16 00:11:33 -0400

    fixed error in setup.sh that caused index.php to not be copied to the correct location

--- auto/setup.sh
+++ auto/setup.sh
@@ -6,6 +6,6 @@ apt-get -y install nginx php5-fpm git
 git clone https://github.com/anishp55/funtime 
 rm -rf /etc/nginx
 cp -r /root/funtime/nginx /etc
-cp /root/funtime/src /usr/share/nginx/html
+cp /root/funtime/src/index.php /usr/share/nginx/html/
 
 rm -rf /root/funtime
