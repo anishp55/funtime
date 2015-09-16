5c8e70fe5f055e6d0187a46cc764d178cb96110a
anish k patel <akp@lp.build>
2015-09-16 00:07:13 -0400

    added setup.sh file to deploy application

---
+++ auto/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y install nginx php5-fpm git
+
+git clone https://github.com/anishp55/funtime 
+rm -rf /etc/nginx
+cp -r /root/funtime/nginx /etc
+cp /root/funtime/src /usr/share/nginx/html
+
+rm -rf /root/funtime
